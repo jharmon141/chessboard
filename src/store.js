2f959a1730e32370646f849531aa7dab76484b05
jharmon141 <jeremy.l.harmon@gmail.com>
2018-03-03 18:30:06 -0600

    store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    selectedTiles: []
+  },
+  getters: {
+    currentTile: state => {
+      return state.selectedTiles[state.selectedTiles.length - 1]
+    }
   },
   mutations: {
-
+    addSelectedTile: (state, params) => {
+      state.selectedTiles.push(params)
+    }
   },
   actions: {
-
+    handleAddSelectedTile: ({ commit} , params) => {
+      commit('addSelectedTile', params)
+    }
   }
 })
