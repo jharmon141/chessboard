d1c80258210feb32b0bcd9ca716bfdbaa7a26b8f
jharmon141 <jeremy.l.harmon@gmail.com>
2018-03-03 18:30:45 -0600

    tile

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,13 +1,37 @@
 <template>
-  
+    <div id="tile"
+        v-on:click="$emit('selectTile')"
+        :class="{ black: black, selected: selected }">
+    </div>
 </template>
 
+
 <script>
+
 export default {
+    name: 'Tile',
+    props: ['column', 'row', 'black'],
+    computed: {
+        selected() {
+            return this.$store.getters.currentTile && this.column === this.$store.getters.currentTile.column && this.row === this.$store.getters.currentTile.row
+        }
+    }
 
 }
 </script>
 
-<style>
 
+<style scoped>
+#tile {
+  width: 12.5%;
+  height: 12.5%;
+}
+
+.black {
+  background-color: black;
+}
+
+.selected {
+  box-shadow: inset 4px 4px limegreen, inset -4px -4px limegreen;
+}
 </style>
